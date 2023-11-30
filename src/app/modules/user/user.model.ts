0398bfd857c291531d205ae00207ceaa79395a98
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-12-01 05:44:26 +0600

    user schema created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model, connect } from 'mongoose';
+import { User } from './user.interface';
+
+const userSchema = new Schema<User>({
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number },
+  email: { type: String },
+  isActive: { type: Boolean, required: true },
+  hobbies: { type: [String], required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  orders: {
+    type: [
+      {
+        productName: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
